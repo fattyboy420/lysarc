cc8990834c662fbdde6740f8b6cbe43988ee9693
fattyboy420 <62025240+fattyboy420@users.noreply.github.com>
2020-09-04 18:22:50 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,3 @@
 function myFunction(){
-var para = document.createElement("p");
-var node = document.createTextNode("Hah lol gotem");
-para.appendChild(node);
-
-var element = document.getElementById("div1");
-var child = document.getElementById("p1");
-element.insertBefore(para,child);
+document.write("Hah lol gotem");
 }
