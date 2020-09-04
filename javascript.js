4b547ffdb2f87d9d89447315683f93553c862e3b
fattyboy420 <62025240+fattyboy420@users.noreply.github.com>
2020-09-04 18:42:08 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
 function myFunction(){
-alert("Hello\nHow are you?");
+var response=window.prompt("Are you ok?(y/n)");
+if(response=="y")
+    {
+        alert("That's nice. Have a nice day!");
+    } 
+  else if(response=="y")
+    {
+        alert("That's not nice. I hope you feel better.");
+    } 
+  else
+  {
+      alert("You have inputted something not y or n. Try again);
+  } 
 }
