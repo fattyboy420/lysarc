d9e2489185f43a0038e61d99342f9776806aeba5
fattyboy420 <62025240+fattyboy420@users.noreply.github.com>
2020-09-05 14:01:47 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,4 @@
-function myFunction(){
-var response=window.prompt("Are you ok?(y/n)");
-if(response=="y")
-    {
-        alert("That's nice. Have a nice day!");
-    } 
-  else if(response=="n")
-    {
-        alert("That's not nice. I hope you feel better.");
-    } 
-  else
-  {
-      alert("You have inputted something not y or n. Try again");
-  } 
-}
+
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
